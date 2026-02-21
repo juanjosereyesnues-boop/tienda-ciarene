ca946e3bbeef79d6c6b2745ce7820b7a2251ba36
juanjosereyesnues-boop <juanjosereyesnues@gmail.com>
2026-02-20 19:35:36 -0500

    Update product names in CATALOGO_ROPA

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def servir_foto(filename):
 CATALOGO_ROPA = [
     {
         "id": 1,
-        "nombre": "Pijama Dama",
+        "nombre": "Pijama Satin",
         "precio": 50000,
         "precio_viejo": 85000,
         "imagen": "https://ciarene.up.railway.app/fotos/pijama.png", # URL pública
@@ -79,6 +79,7 @@ def get_productos():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
+
 
 
 
