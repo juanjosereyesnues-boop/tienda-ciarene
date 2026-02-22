3034e3ab910ac4e16963f9984201c85fb76b3fb8
juanjosereyesnues-boop <juanjosereyesnues@gmail.com>
2026-02-21 23:30:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, jsonify, send_from_directory
 import os
 
-app = Flask(__name__, static_folder='.')
+app = Flask(__name__)
 
-# Catálogo simple
 CATALOGO = [
     {"id": 1, "nombre": "Pijama Satin Luxe", "precio": 55000, "imagen": "https://images.unsplash.com/photo-1583321500900-82807e458f3c?w=500"},
     {"id": 2, "nombre": "Conjunto Power Gym", "precio": 70000, "imagen": "https://images.unsplash.com/photo-1518310383802-640c2de311b2?w=500"},
@@ -12,17 +11,13 @@ CATALOGO = [
 
 @app.route('/')
 def home():
-    return send_from_directory('.', 'index.html')
-
-@app.route('/carrito')
-def cart():
-    return send_from_directory('.', 'carrito.html')
+    # Buscamos el archivo en la carpeta actual
+    return send_from_directory(os.getcwd(), 'index.html')
 
 @app.route('/api/productos')
 def api():
     return jsonify(CATALOGO)
 
 if __name__ == '__main__':
-    # Railway necesita esto sí o sí
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
