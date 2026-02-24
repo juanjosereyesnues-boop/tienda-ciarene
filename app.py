ab40fd16f64c9c96184495e5c7880f92dba1455b
juanjosereyesnues-boop <juanjosereyesnues@gmail.com>
2026-02-24 15:21:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ CATALOGO = [
     {"id": 1, "nombre": "Camiseta Ciarené", "precio": 45000, "precio_antes":62000, "imagen": "camisetas.png", "categoria": "Camisas"},
     {"id": 2, "nombre": "Pantalón Premium", "precio": 85000, "imagen": "pantalon.png", "categoria": "Pantalones"},
     {"id": 3, "nombre": "Pijama Seda", "precio": 65000, "imagen": "pijama.png", "categoria": "Casual"},
-    {"id": 4, "nombre": "Sudadera Sport", "precio": 75000, "imagen": "sudadera.png", "categoria": "Deportivo"}
+    {"id": 4, "nombre": "Sudadera Sport", "precio": 75000, "imagen": "sudadera.png", "categoria": "Deportivo"},
     {"id": 5, "nombre": "Pijama Estilo", "precio": 35000, "imagen": "Pijama2.pnej, "categoria": "Casual"}
 ]
 
@@ -32,6 +32,7 @@ def send_report(path):
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
 
 
